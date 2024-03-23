6aaf9209837c761624ef9a0e5a18ca34964d37ac
Boom SP <boom.sorasiri@gmail.com>
2024-03-23 14:25:07 +0900

    error fixes

--- kali_setup.sh
+++ kali_setup.sh
@@ -12,7 +12,7 @@ chrome_deb=$(echo "${chrome_url}" | rev | cut -d'/' -f 1 | rev)
 
 # Dependency
 sudo apt install -y libu2f-udev
-wget "${chrome_url}" ~/Downloads/"${chrome_deb}"
+wget -P ~/Downloads/"${chrome_deb}" "${chrome_url}"
 sudo dpkg -i ~/Downloads/"${chrome_deb}"
 rm "${chrome_deb}"
 echo "[+] Google Chrome Installed"
@@ -39,7 +39,7 @@ sudo apt install -y openssh-server
 # Install pip2
 pip2_url="https://bootstrap.pypa.io/pip/2.7/get-pip.py"
 pip2_py=$(echo "${pip2url}" | rev | cut -d'/' -f 1 | rev)
-wget "${pip2_url}" ~/Downloads/"${pip2_py}"
+wget -P ~/Downloads/"${pip2_py}" "${pip2_url}"
 sudo python2 ~/Downloads/"${pip2_py}"
 pip2 install --upgrade setuptools
 sudo apt-get install -y python-dev
@@ -67,12 +67,12 @@ mkdir ~/tools
 # Get ysoserial
 ysoserial_url="https://github.com/frohoff/ysoserial/releases/download/v0.0.6/ysoserial-all.jar"
 ysoserial_jar=$(echo "${ysoserial_url}" | rev | cut -d'/' -f 1 | rev)
-wget "${ysoserial_url}" ~/tools/"${ysoserial_jar}"
+wget -P ~/tools/"${ysoserial_jar}" "${ysoserial_url}"
 mv ~/tools/"$ysoserial_jar" ~/tools/ysoserial.jar
 
 # Setup minimal nvim
 nvim_init_url="https://raw.githubusercontent.com/BoomSP/setup-and-dotfiles/main/init.nvim"
-wget "${nvim_init_url}" ~/.config/nvim/init.nvim
+wget -P ~/.config/nvim/init.nvim"${nvim_init_url}"
 
 sudo apt -y autoremove
 
